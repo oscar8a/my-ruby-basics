025f827454a6eb1d79b8b603762012c81097b933
github username <oscar8atech@gmail.com>
2019-11-12 16:29:01 -0500

    array selection

--- rubycode.rb
+++ rubycode.rb
@@ -1,31 +1,25 @@
 # Ruby Basics
 
-# Loops
-  # while
-  x = 4
-  while x >= 1 
-    puts "x is #{x}"
-    x = x - 1
+# Array Selection
+  arr = [-10, -7, -6, -4, -3, 0, 1, 2, 4, 5, 8, 9, 11]
+  def select_arr(arr)
+    # select and return all odd numbers from the Array variable `arr`
+      arr.select {|num| num % 2 != 0}
   end
-
-  # for
-  i = "I am i"
-  for a in 1..5 do   
-    puts i
+  
+  def reject_arr(arr)
+    # reject all elements which are divisible by 3
+      arr.reject {|x| x % 3 == 0}
   end
-
-  #do..while
-  loop do
-    puts "doing..."
-    val = '7'
-    if val == '7'
-     break
-    end
+  
+  def delete_arr(arr)
+    # delete all negative elements
+      arr.delete_if {|z| z < 0}
   end
-
-  #until
-  var = 7
-  until var == 11 do
-    puts var * 10
-    var = var + 1
+  
+  def keep_arr(arr)
+    # keep all non negative elements ( >= 0)
+      arr.keep_if {|pos| pos >= 0}
   end
+  
+  # puts keep_arr(arr)
