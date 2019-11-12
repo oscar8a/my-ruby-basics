6c7c72dcb9ec1ddf5852158163d4157a2f0cf3d4
github username <oscar8atech@gmail.com>
2019-11-12 16:09:24 -0500

    ruby basics file

---
+++ rubycode.rb
@@ -0,0 +1,33 @@
+# Ruby Basics
+
+# Loops
+  # while
+  x = 4
+  while x >= 1 
+    puts "x is #{x}"
+    x = x - 1
+  end
+
+  # for
+  i = "I am i"
+  for a in 1..5 do   
+    puts i
+  end
+
+  #do..while
+  loop do
+    puts "doing..."
+    val = '7'
+    if val == '7'
+     break
+    end
+  end
+
+  #until
+  var = 7
+  until var == 11 do
+    puts var * 10
+    var = var + 1
+  end
+
+  
